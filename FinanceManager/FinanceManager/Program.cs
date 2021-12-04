025dfc86289cd6c8fa0b7a90808eef77cf71a434
mamonich <mamonich@users.noreply.github.com>
2021-12-04 20:25:26 +0200

    deleting some choices

--- FinanceManager/FinanceManager/Program.cs
+++ FinanceManager/FinanceManager/Program.cs
@@ -16,45 +16,37 @@ namespace FinanceManager
                 switch (numberOfChoice)
                 {
                     case 1 :
-                        Console.WriteLine("Напишите путь к файлу");
-                        string filePath = Console.ReadLine();
-                        financeManager.FinanceReportRepository.SetFilePath(filePath);
-                        break;
-                    case 2 :
-                        financeManager.FinanceReportRepository.SafeFile();
-                        break;
-                    case 3 :
                         Console.WriteLine("Напишите параметры таким образом(описание;сумма;дата;тип записи;учавствует ли в подсчётах(да,нет))");                 
                         financeManager.AddFinanceReport(Console.ReadLine());
                         break;
-                    case 4 :
+                    case 2 :
                         Console.WriteLine("Укажите номер записи, которую желаете удалить");
                         financeManager.DeleteFinanceReport(int.Parse(Console.ReadLine()));
                         break;
-                    case 5 :
+                    case 3 :
                         Console.WriteLine("Укажите номер записи, которую желаете редактировать");
                         string idString = Console.ReadLine();
                         Console.WriteLine("Напишите параметры таким образом(описание;сумма;дата;тип записи;учавствует ли в подсчётах(да,нет))");
-
                         financeManager.ChangeFinanceReport(int.Parse(idString), Console.ReadLine());
                         break;
-                    case 6 :
+                    case 4 :
                         Console.WriteLine("Напишите дату(день, месяц, год), с которой начнётся подсчёт");
                         string StartDateString = Console.ReadLine();
                         Console.WriteLine("Напишите дату(день, месяц, год), на которой закончится подсчёт");
                         string EndDateString = Console.ReadLine();
                         financeManager.CalculateSumForDates(StartDateString, EndDateString);
                         break;
-                    case 7 :
+                    case 5 :
                        financeManager.CalculateSumForWeek();
                         break;
-                    case 8 :
+                    case 6 :
                         financeManager.CalculateSumForMonth();
                         break;
-                    case 9:
+                    case 7:
                         financeManager.showFinanceReports();
                         break;
                     case 0:
+                        financeManager.FinanceReportRepository.SafeFile();
                         break;
 
                     default:
